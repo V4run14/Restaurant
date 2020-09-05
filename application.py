f061a922612c5c1bd5d54daf8f1b93d36ee5e77e
V4run14 <59575040+V4run14@users.noreply.github.com>
2020-09-06 01:03:34 +0530

    Regex

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import os
-
+import re
 from flask import Flask, render_template, request, redirect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -18,13 +18,35 @@ def index():
 @app.route("/login", methods=["POST"])
 def login():
     #return "Login page"
-    return redirect("/register", code=307)
-    # Get form information.
-    #name = request.form.get("name")
+    email = request.form.get("email")
+    x = re.search("@manager.com" , email)
+    y = re.search("@delivery.com" , email)
+    if email == "admin@admin.com":
+        return redirect("/admin", code=307)
+    elif x:
+        return redirect("/manager", code=307)
+    elif y:
+        return redirect("/delivery", code=307)
+    else:
+        return redirect("/cust", code=307)
+    
 
 @app.route("/register", methods=["POST"])
 def register():
     return "you have been redirected"
-    #return render_template("index.html")
-    # Get form information.
-    #name = request.form.get("name")
+
+@app.route("/admin", methods=["POST"])
+def admin():
+    return "This is the admin view"
+
+@app.route("/manager", methods=["POST"])
+def manager():
+    return "This is the manager view"
+
+@app.route("/cust", methods=["POST"])
+def cust():
+    return "This is the customer view"
+
+@app.route("/delivery", methods=["POST"])
+def delivery():
+    return "This is the delivery boy view"
