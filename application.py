547fb2a99c8be8c443679cbf1608eccd3ed19086
Kush Dang <kushdang@gmail.com>
2020-10-08 14:39:25 +0530

    Signin page

--- application.py
+++ application.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+#engine = create_engine(os.getenv("DATABASE_URL"))
+#db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
